a830ef550125a89bd11b7a264695a33bf41312db
JoshHerde <jherde2@wgu.edu>
2023-04-12 12:06:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myandroidapp/MainScreen.java
@@ -0,0 +1,26 @@
+package com.example.myandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+
+        Button termsButton = findViewById(R.id.termsButton);
+        termsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( );
+            }
+        });
+
+    }
+}
